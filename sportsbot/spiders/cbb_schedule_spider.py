400ebdb2f20eae8144087cbdf5f951f59e8eda6a
Jack <jpick329@gmail.com>
2025-01-04 21:42:15 -0500

    use get

--- sportsbot/spiders/cbb_schedule_spider.py
+++ sportsbot/spiders/cbb_schedule_spider.py
@@ -10,17 +10,17 @@ class CbbScheduleSpider(scrapy.Spider):
     def parse(self, response):
         for row in response.xpath('//table[@id="schedule"]/tbody/tr'):
             yield {
-                'Date': row.xpath('.//td[@data-stat="date_game"]/a/text()').extract_first(),
-                'Time': row.xpath('.//td[@data-stat="time_game"]/text()').extract_first(),
-                'Opponent': row.xpath('.//td[@data-stat="opp_name"]/a/text()').extract_first(),
-                'Conference': row.xpath('.//td[@data-stat="conf_abbr"]/a/text()').extract_first(),
-                'SRS': row.xpath('.//td[@data-stat="srs"]/text()').extract_first(),
-                'Result': row.xpath('.//td[@data-stat="game_result"]/text()').extract_first(),
-                'Team Points': row.xpath('.//td[@data-stat="pts"]/text()').extract_first(),
-                'Opp Points': row.xpath('.//td[@data-stat="opp_pts"]/text()').extract_first(),
-                'OT': row.xpath('.//td[@data-stat="overtimes"]/text()').extract_first(),
-                'W': row.xpath('.//td[@data-stat="wins"]/text()').extract_first(),
-                'L': row.xpath('.//td[@data-stat="losses"]/text()').extract_first(),
-                'Streak': row.xpath('.//td[@data-stat="game_streak"]/text()').extract_first(),
-                'Arena': row.xpath('.//td[@data-stat="arena"]/text()').extract_first()
+                'Date': row.xpath('.//td[@data-stat="date_game"]/a/text()').get(),
+                'Time': row.xpath('.//td[@data-stat="time_game"]/text()').get(),
+                'Opponent': row.xpath('.//td[@data-stat="opp_name"]/a/text()').get(),
+                'Conference': row.xpath('.//td[@data-stat="conf_abbr"]/a/text()').get(),
+                'SRS': row.xpath('.//td[@data-stat="srs"]/text()').get(),
+                'Result': row.xpath('.//td[@data-stat="game_result"]/text()').get(),
+                'Team Points': row.xpath('.//td[@data-stat="pts"]/text()').get(),
+                'Opp Points': row.xpath('.//td[@data-stat="opp_pts"]/text()').get(),
+                'OT': row.xpath('.//td[@data-stat="overtimes"]/text()').get(),
+                'W': row.xpath('.//td[@data-stat="wins"]/text()').get(),
+                'L': row.xpath('.//td[@data-stat="losses"]/text()').get(),
+                'Streak': row.xpath('.//td[@data-stat="game_streak"]/text()').get(),
+                'Arena': row.xpath('.//td[@data-stat="arena"]/text()').get()
             }
